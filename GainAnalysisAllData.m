ecc64f784336b579b41c8aee5d3763653f87f779
Srinivas Gorur-Shandilya <srinivas.gs@yale.edu>
2014-08-19 13:25:36 -0400

    finished a peak-based gain analysis for the fast odor

--- GainAnalysisAllData.m
+++ GainAnalysisAllData.m
@@ -337,6 +337,50 @@ disp(gof.rsquare)
 % In contrast, the r-square of the fit to stimulus peak vs. blank duration is:
 disp(rsquare(ValveOffDurations,PIDPeaks))
 
+%%
+% The plots above show that the longer the low stimulus period is preceding the current pulse, the large is the response of the neuron, given a constant pulse height. The duration of the preceding blank is really a proxy for what we are really interested in, which is the mean stimulus delivered to the neuron. In the following figure, we plot the gain (peak response/stimulus amplitude) as a function of the mean stimulus in the preceding 500ms.
+
+[ons,offs] = ComputeOnsOffs(data(td).Valve);
+ORNPeaks = NaN(1,length(ons));
+PIDPeaks = NaN(1,length(ons));
+Gain =  NaN(1,length(ons));
+MeanStimulus = NaN(1,length(ons));
+
+w = floor(0.5/3e-3);
+s = find(ons>w,1,'first')+1;
+
+for i = s:length(ons)
+	ORNPeaks(i)=max(data(td).ORN(ons(i):offs(i)));
+	PIDPeaks(i)=max(data(td).PID(ons(i):offs(i)));
+	MeanStimulus(i) = mean(data(td).PID(ons(i)-w:ons(i)));
+end
+
+Gain = ORNPeaks./PIDPeaks;
+ORNPeaks(1:s-1) = [];
+PIDPeaks(1:s-1) = [];
+Gain(1:s-1) = [];
+MeanStimulus(1:s-1) = [];
+
+% fit a line
+[ffit, gof] = fit(MeanStimulus(:),ORNPeaks(:),'Poly1');
+x = sort(unique(MeanStimulus));
+y = ffit(x);
+
+
+figure('outerposition',[0 0 500 500],'PaperUnits','points','PaperSize',[1000 500]); hold on
+plot(MeanStimulus,ORNPeaks,'k.','MarkerSize',24)
+plot(x,y,'r')
+ylabel('Peak response (Hz)')
+xlabel('Mean Stimulus in past 500ms')
+
+
+PrettyFig;
+
+snapnow;
+delete(gcf);
+
+
+
 return
 
 % ##       ##    ##     ######      ###    #### ##    ##       ###    ##    ##    ###    ##       

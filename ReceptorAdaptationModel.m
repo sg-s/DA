7fbb85a3611c845a8cbea140d3e796ae5e965d1d
Srinivas Gorur-Shandilya <srinivas.gs@yale.edu>
2014-12-08 13:32:39 -0500

    wrote a model for receptor adaptation

---
+++ ReceptorAdaptationModel.m
@@ -0,0 +1,75 @@
+% ReceptorAdaptationModel.m
+% this is a model of ORN response where the ORN response f is given by
+% f = N(Rx(s/(s+Kxs)))
+% where
+% s is the stimulus
+% K is a filter that governs how the receptors adapt
+% R is a filter that governs the impulse response function of the neuron
+% N is a hill function
+% and x is meant to indicate convolution
+% K and R can be any parametrised filter
+% 
+% usage:
+% f = ReceptorAdaptationModel(s,p)
+% where s is a stimulus vector
+% and p is a structure with the parameters of this model defined e.g.:
+% p.Kr = @filter_gamma2;
+% p.Ka = @filter_gamma2;
+% p.Kr_tau1 = 10;
+% p.Kr_n = 2;
+% p.Kr_tau2 = 20;
+% p.Kr_A = 1;
+% p.Ka_tau1 = 15;
+% p.Ka_n = 2;
+% p.Ka_tau2 = 25;
+% p.Ka_A = 2;
+% p.A = 100; % these are the parameters for the hill function at the output
+% p.n = 2;
+% p.Kd = 5;
+% p.offset = 2;
+% % 
+% created by Srinivas Gorur-Shandilya at 10:52 , 08 December 2014. Contact me at http://srinivas.gs/contact/
+% 
+% This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
+% To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+function f = ReceptorAdaptationModel(s,p)
+switch nargin
+case 0
+	help ReceptorAdaptationModel
+	return
+case 1
+	error('No parameters specified')
+end
+
+% revert to default filters if none specified
+if ~isfield(p,'Ka')
+	p.Ka = @filter_gamma2;
+end
+if ~isfield(p,'Kr')
+	p.Kr = @filter_gamma2;
+end
+
+% make the filters
+t = 1:1000;
+Kr = p.Kr(p.Kr_tau1,p.Kr_n,p.Kr_tau2,p.Kr_A,t);
+Ka = p.Ka(p.Ka_tau1,p.Ka_n,p.Ka_tau2,p.Ka_A,t);
+
+% filter the stimulus
+shat = s./(s + filter(Ka,1,s));
+shat = filter(Kr,1,shat);
+
+% pass through output non-linearity
+x = [p.A p.Kd p.n p.offset];
+f = hill4(x,shat);
+
+
+
+
+
+
+
+
+
+
+
+

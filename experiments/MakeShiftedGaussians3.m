3d9b908022aca87b0a9a7271e3ba9554e5948ab6
srinivas.gs <srinivas.gs@yale.edu>
2015-03-02 10:59:44 -0500

    bug fix

--- experiments/MakeShiftedGaussians3.m
+++ experiments/MakeShiftedGaussians3.m
@@ -9,14 +9,13 @@
 % ControlParadigm Order (should match setup, and config. in Kontroller)
 %
 % 1. (AO)   to LED Dimmer (0-10V)
-% 2. (AO)   to MFC200 (0-5V)
 % 3. (AO)   to MFC500 (0-5V)
 % 2. (DO)   to switch controlling main air @ 2L/min
 
 clear ControlParadigm
 ControlParadigm(1).Name = 'CleanAir';
-ControlParadigm(1).Outputs = zeros(4,1e4);
-ControlParadigm(1).Outputs(4,:) = 1;
+ControlParadigm(1).Outputs = zeros(3,1e4);
+ControlParadigm(1).Outputs(3,:) = 1;
 
 
 dt  =1e-4;
@@ -46,16 +45,16 @@ i = 2;
 n = strcat('MFC500-.5V');
 ControlParadigm2(i).Name = n;
 
-ControlParadigm2(i).Outputs= zeros(4,T/dt);
+ControlParadigm2(i).Outputs= zeros(3,T/dt);
 
-[~,ControlParadigm2(i).Outputs(3,:)] = BestDistribution([],p);
+[~,ControlParadigm2(i).Outputs(2,:)] = BestDistribution([],p);
     
-ControlParadigm2(i).Outputs(3,end) = mean(ControlParadigm2(i).Outputs(3,:)); % don't leave the ORN with some high or low value
-ControlParadigm2(i).Outputs(3,ControlParadigm2(i).Outputs(3,:) > 5) = 5; % clip for sanity
-ControlParadigm2(i).Outputs(3,ControlParadigm2(i).Outputs(3,:) < 0) = 0; % clip for sanity
+ControlParadigm2(i).Outputs(2,end) = mean(ControlParadigm2(i).Outputs(3,:)); % don't leave the ORN with some high or low value
+ControlParadigm2(i).Outputs(2,ControlParadigm2(i).Outputs(2,:) > 5) = 5; % clip for sanity
+ControlParadigm2(i).Outputs(2,ControlParadigm2(i).Outputs(2,:) < 0) = 0; % clip for sanity
 
 % add the main air
-ControlParadigm2(i).Outputs(4,:) = 1;
+ControlParadigm2(i).Outputs(3,:) = 1;
 
 
 ControlParadigm = [ControlParadigm ControlParadigm2];
@@ -63,7 +62,7 @@ ControlParadigm = [ControlParadigm ControlParadigm2];
 l = length(ControlParadigm)+1;
 
 ControlParadigm(l).Name = 'end';
-ControlParadigm(l).Outputs = zeros(4,1e4);
+ControlParadigm(l).Outputs = zeros(3,1e4);
 
 %% save
 n = ('c:\srinivas\ShapedGaussianFlicker_Kontroller_paradigm.mat');

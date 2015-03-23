a07937513b20a7e4a88712bfd351f5272c112090
srinivas.gs <srinivas.gs@yale.edu>
2015-03-23 10:35:52 -0400

    removed more cruft

--- LNCostFunction.m
+++
@@ -1,8 +0,0 @@
-function cost = LNCostFunction(x,data)
-% unpack data
-response = data.response(:);
-
-Rguess = SolveLNModel(x,data.stimulus,data.response);
-
-% calculate the cost
-cost = Cost2(response(300:end),Rguess(300:end));

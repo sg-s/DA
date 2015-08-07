509aceeb7502e90fd007dfa35315f8f6ea38ca5f
srinivas.gs <srinivas.gs@yale.edu>
2015-08-07 10:53:56 -0400

    added a more sophisticated analysis of natural flickering

--- NaturalFlickering.m
+++ NaturalFlickering.m
@@ -315,7 +315,7 @@ end
 %            ##     ## ##    ## ##     ## ########    ##     ######  ####  ######  
 
 %% Output Analysis
-% In this section, we analyse the prediction of the linear kernel in some more detail. The following figure shows a plot of the linear predictions vs. the actual response. We colour the data by the mean stimulus in the preceding 500ms. 
+% In this section, we analyse the prediction of the linear kernel in some more detail. The following figure shows a plot of the linear predictions vs. the actual response. We colour the data by the mean stimulus in the preceding 500ms.  We see that each excursion has a different slope, and it looks like the slopes correspond to the stimulus in the preceding 500ms. Larger mean stimulus in the preceding 500ms is indicated by brighter colours. In the second subplot, we plot the gain of each of these excursions vs the mean stimulus in the preceding 500ms. The red line is a power-law fit, and the exponent is mentioned in the fit legend. 
 
 shat = ComputeSmoothedStimulus(mean2(PID),500);
 % shat(fp<10) = 0;
@@ -325,7 +325,8 @@ shat = 1+ceil(shat*99);
 shat(isnan(shat)) = 1;
 
 ss = 1;
-figure('outerposition',[0 0 500 500],'PaperUnits','points','PaperSize',[1000 500]); hold on
+figure('outerposition',[0 0 1000 500],'PaperUnits','points','PaperSize',[1000 500]); hold on
+subplot(1,2,1), hold on
 
 cc = parula(100);
 c= cc(shat,:);
@@ -333,6 +334,37 @@ scatter(fp(1:ss:end),R(1:ss:end),[],c(1:ss:end,:),'filled')
 
 xlabel('Linear Prediction (Hz)')
 ylabel('Actual response (Hz)')
+
+subplot(1,2,2), hold on
+fp = convolve(tA,mean2(PID),K,filtertime);
+shat = ComputeSmoothedStimulus(mean2(PID),500);
+
+% find all excursions (defined as firing rate crossing 10Hz)
+[whiff_starts,whiff_ends] = ComputeOnsOffs(R>10);
+mean_stim = NaN*whiff_ends;
+gain = NaN*whiff_ends;
+gain_err =  NaN*whiff_ends;
+for i = 1:length(whiff_ends)
+	mean_stim(i) = mean(shat(whiff_starts(i):whiff_ends(i)));
+	ff=fit(fp(whiff_starts(i):whiff_ends(i)),R(whiff_starts(i):whiff_ends(i)),'poly1');
+	gain(i) = ff.p1;
+	temp = confint(ff);
+	gain_err(i) = diff(temp(:,1))/2;
+end
+rm_this = (abs(gain_err./gain)) > .5; % throw out points where the estimate of gain has a more than 50% error
+gain(rm_this) = [];
+gain_err(rm_this) = [];
+mean_stim(rm_this) = [];
+
+errorbar(mean_stim,gain,gain_err,'k+')
+
+ff = fit(mean_stim(:),gain(:),'power1','Weights',1./gain_err);
+
+l = plot(sort(mean_stim),ff(sort(mean_stim)),'r')
+L = strcat('\alpha=',oval(ff.b));
+legend(l,L)
+xlabel('Mean Stimulus in preceding 500ms (V)')
+ylabel('Gain (Hz/V)')
 
 PrettyFig;
 
@@ -342,8 +374,8 @@ if being_published
 end
 
 
-%% 
-% We see that each excursion has a different slope, and it looks like the slopes correspond to the stimulus in the preceding 500ms. Larger mean stimulus in the preceding 500ms is indicated by brighter colours. 
+
+
 
 %% Version Info
 % The file that generated this document is called:

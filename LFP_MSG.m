1952f30328ed22af2242eb6ff8dac18f8cf3f44c
srinivas.gs <srinivas.gs@yale.edu>
2015-09-15 11:32:58 -0400

    fixed #253, #258

--- LFP_MSG.m
+++ LFP_MSG.m
@@ -131,9 +131,9 @@ end
 %% Gain in Firing rate
 % We now look at how the firing rate gain changes with changes in the mean of the stimulus. We want to check if we can reproduce our earlier results, where we showed that the gain of the firing rates goes down with increasing mean stimulus, and is well fit by a power law with exponent close to -1. 
 
-
 a = 10e3; z = 55e3;
 [K2,fA_pred,fA_gain,fA_gain_err] = extractFilters(PID,fA,'use_cache',true,'a',a,'z',z);
+
 
 ss = 50;
 c = parula(max(paradigm)+1);
@@ -172,8 +172,21 @@ e = fA_gain_err(:);
 rm_this = isnan(x) | isnan(y) | isnan(e);
 x(rm_this) = []; y(rm_this) = []; e(rm_this) = [];
 ff = fit(x(:),y(:),'power1','Weights',1./e);
-l = plot(sort(x),ff(sort(x)),'k--');
-legend(l,['y = \alpha (x^\beta) ,\beta = ', oval(ff.b)  ,' ,r^2=' oval(rsquare(ff(x),y))])
+clear l
+l(1) = plot(sort(x),ff(sort(x)),'k--');
+L = {};
+L{1} = ['y = \alpha (x^\beta) ,\beta = ', oval(ff.b)  ,' ,r^2=' oval(rsquare(ff(x),y))];
+
+
+fo = fitoptions('power1');
+fo.Upper = [NaN -1];
+fo.Lower = [NaN -1];
+fo.Weights = 1./e;
+ff = fit(x(:),y(:),'power1',fo);
+l(2) = plot(sort(x),ff(sort(x)),'k');
+L{2} = ['y = \alpha (x^\beta) ,\beta := -1 ,r^2=' oval(rsquare(ff(x),y))];
+
+legend(l,L)
 
 set(gca,'XScale','log','YScale','log','YLim',[10 200])
 xlabel('Mean Stimulus (V)')
@@ -209,49 +222,40 @@ end
 %% Dynamics of Gain Control in Firing Rates
 % In this section we analyse how gain varies as a function of time, for each of the paradigms, for all neurons. We want to see if the ORN firing rate gain varies as a function of time during the time course of each stimulus presentation. 
 
-a = 5;
-z = 50;
+a = 6;
+z = 51;
 window_length = 5;
+sliding_window = 1;
 
 assert(isint((z-a)/window_length),'Choose parameters so that you have an integer number of bins');
+assert(isint((z-a)/sliding_window),'Choose parameters so that you have an integer number of bins');
 
-sliding_fA_gain = NaN((z-a)/window_length,width(PID));
-
-
-for i = 1:width(PID)
-	for j = a:5:z
-		x = fA_pred(j*1e3:(j+window_length)*1e3,i);
-		y = fA(j*1e3:(j+window_length)*1e3,i);
-		try
-			ff = fit(x(:),y(:),'poly1');
-			sliding_fA_gain(floor(j/window_length),i) = ff.p1;
-		catch
+sliding_fA_gain = cache(dataHash([fA(:); fA_pred(:); a; z; sliding_window; window_length]));
+if isempty(sliding_fA_gain)
+	sliding_fA_gain = NaN(length(a:sliding_window:z),width(PID));
+	for i = 1:width(PID)
+		for j = a:sliding_window:z
+			x = fA_pred(j*1e3:(j+window_length)*1e3,i);
+			y = fA(j*1e3:(j+window_length)*1e3,i);
+			try
+				ff = fit(x(:),y(:),'poly1');
+				sliding_fA_gain(ceil((j-a)/sliding_window),i) = ff.p1;
+			catch
+			end
 		end
 	end
+	cache(dataHash([fA(:); fA_pred(:); a; z; sliding_window; window_length]),[]);
+	cache(dataHash([fA(:); fA_pred(:); a; z; sliding_window; window_length]),sliding_fA_gain);
 end
 
 figure('outerposition',[0 0 1000 500],'PaperUnits','points','PaperSize',[1000 500]); hold on
-subplot(1,2,1), hold on
 for i = 1:max(paradigm)
 	temp=nanmean(sliding_fA_gain(:,paradigm==i),2);
 	n = width(sliding_fA_gain(:,paradigm==i));
 	temp2=nanstd(sliding_fA_gain(:,paradigm==i)')/(sqrt(n));
-	errorbar((a:window_length:z)+window_length/2,temp,temp2,'Color',c(i,:))
+	errorShade((a:sliding_window:z)+sliding_window/2,temp,temp2,'Color',c(i,:));
 end
 ylabel('Gain (Hz/V)')
-xlabel('Time (s)')
-
-subplot(1,2,2), hold on
-for i = 1:max(paradigm)
-	temp=nanmean(sliding_fA_gain(:,paradigm==i),2);
-	
-	n = width(sliding_fA_gain(:,paradigm==i));
-	temp2=nanstd(sliding_fA_gain(:,paradigm==i)')/(sqrt(n));
-	temp2 = temp2/temp(1);
-	temp = temp/temp(1);
-	errorbar((a:window_length:z)+window_length/2,temp,temp2,'Color',c(i,:))
-end
-ylabel('Gain (norm)')
 xlabel('Time (s)')
 
 prettyFig;
@@ -360,7 +364,6 @@ for i = 1:max(paradigm)
 	my = mean(mean(y));
 	for j = 1:width(y)
 		y(:,j) = bandPass(y(:,j),1e3,10);
-		% y(:,j) = y(:,j) - mean(y(:,j));
 	end
 	y= y+my;
 	y = mean2(y);
@@ -370,7 +373,7 @@ for i = 1:max(paradigm)
 end
 legend(l,{AllControlParadigms.Name},'Location','southeastoutside')
 xlabel('Linear Prediction')
-ylabel('PID \rightarrow LFP Filter')
+ylabel('LFP (mV)')
 
 subplot(1,10,7:10), hold on
 
@@ -378,16 +381,27 @@ for i = 1:width(LFP)
 	errorbar(mean(PID(a:z,i)),LFP_gain(i),LFP_gain_err(i),'+','Color',c(paradigm(i),:))
 end
 
-% fit a power law to this
 x = mean(PID(a:z,:)); x = x(:);
 y = LFP_gain(:);
 e = LFP_gain_err(:);
 rm_this = isnan(x) | isnan(y) | isnan(e);
 x(rm_this) = []; y(rm_this) = []; e(rm_this) = [];
 ff = fit(x(:),y(:),'power1','Weights',1./e);
-l = plot(sort(x),ff(sort(x)),'k--');
-legend(l,['y = \alpha (x^\beta) ,\beta = ', oval(ff.b)  ,' ,r^2=' oval(rsquare(ff(x),y))])
+clear l
+l(1) = plot(sort(x),ff(sort(x)),'k--');
+L = {};
+L{1} = ['y = \alpha (x^\beta) ,\beta = ', oval(ff.b)  ,' ,r^2=' oval(rsquare(ff(x),y))];
 
+
+fo = fitoptions('power1');
+fo.Upper = [NaN -1];
+fo.Lower = [NaN -1];
+fo.Weights = 1./e;
+ff = fit(x(:),y(:),'power1',fo);
+l(2) = plot(sort(x),ff(sort(x)),'k');
+L{2} = ['y = \alpha (x^\beta) ,\beta := -1 ,r^2=' oval(rsquare(ff(x),y))];
+
+legend(l,L)
 set(gca,'XScale','log','YScale','log')
 xlabel('Mean Stimulus (V)')
 ylabel('LFP Gain')
@@ -417,49 +431,42 @@ end
 %          #######  ##           ######   ##     ## #### ##    ## 
 
 %% Dynamics of Gain Control
-% In this section we analyse how gain varies as a function of time, for each of the paradigms, for all neurons. We are trying to quantify the effect we saw before, which was that gain seems to decrease over time as we present the stimulus. In this section, we estimate the gain in 5 second non-overlapping blocks.
+% In this section we analyse how gain varies as a function of time, for each of the paradigms, for all neurons. We are trying to quantify the effect we saw before, which was that gain seems to decrease over time as we present the stimulus. In this section, we estimate the gain in 5 second overlapping blocks that slide by 1 second.
 
 a = 5;
 z = 50;
 window_length = 5;
+sliding_window = 1;
 
 assert(isint((z-a)/window_length),'Choose parameters so that you have an integer number of bins');
+assert(isint((z-a)/sliding_window),'Choose parameters so that you have an integer number of bins');
 
-sliding_LFP_gain = NaN((z-a)/window_length,width(PID));
-
-
-for i = 1:width(PID)
-	for j = a:5:z
-		x = LFP_pred(j*1e3:(j+window_length)*1e3,i);
-		y = LFP(j*1e3:(j+window_length)*1e3,i);
-		try
-			ff = fit(x(:),y(:),'poly1');
-			sliding_LFP_gain(floor(j/window_length),i) = ff.p1;
-		catch
+sliding_LFP_gain = cache(dataHash([LFP(:); LFP_pred(:); a; z; sliding_window; window_length]));
+if isempty(sliding_LFP_gain)
+	sliding_LFP_gain = NaN(length(a:sliding_window:z),width(PID));
+	for i = 1:width(PID)
+		for j = a:sliding_window:z
+			x = LFP_pred(j*1e3:(j+window_length)*1e3,i);
+			y = LFP(j*1e3:(j+window_length)*1e3,i);
+			try
+				ff = fit(x(:),y(:),'poly1');
+				sliding_LFP_gain(ceil((j-a)/sliding_window),i) = ff.p1;
+			catch
+			end
 		end
 	end
+	cache(dataHash([LFP(:); LFP_pred(:); a; z; sliding_window; window_length]),[]);
+	cache(dataHash([LFP(:); LFP_pred(:); a; z; sliding_window; window_length]),sliding_LFP_gain);
 end
 
 figure('outerposition',[0 0 1000 500],'PaperUnits','points','PaperSize',[1000 500]); hold on
-subplot(1,2,1), hold on
 for i = 1:max(paradigm)
 	temp=nanmean(sliding_LFP_gain(:,paradigm==i),2);
 	n = width(sliding_LFP_gain(:,paradigm==i));
 	temp2=nanstd(sliding_LFP_gain(:,paradigm==i)')/(sqrt(n));
-	errorbar((a:window_length:z)+window_length/2,temp,temp2,'Color',c(i,:))
+	errorShade((a:sliding_window:z)+sliding_window/2,temp,temp2,'Color',c(i,:));
 end
 ylabel('Gain (mV/V)')
-xlabel('Time (s)')
-
-subplot(1,2,2), hold on
-for i = 1:max(paradigm)
-	temp=nanmean(sliding_LFP_gain(:,paradigm==i),2);
-	temp = temp/temp(1);
-	n = width(sliding_LFP_gain(:,paradigm==i));
-	temp2=nanstd(sliding_LFP_gain(:,paradigm==i)')/(sqrt(n));
-	errorbar((a:window_length:z)+window_length/2,temp,temp2,'Color',c(i,:))
-end
-ylabel('Gain (norm)')
 xlabel('Time (s)')
 
 prettyFig;
@@ -468,6 +475,41 @@ if being_published
 	snapnow
 	delete(gcf)
 end
+
+%%
+% We now fit power laws to each of these time points, to see if the reason we get an exponent that is not -1 is somehow time dependant. 
+
+sliding_LFP_exponent = NaN(length(a:sliding_window:z),1);
+sliding_LFP_exponent_err = sliding_LFP_exponent;
+
+windows = a:sliding_window:z;
+
+for i = 1:length(sliding_LFP_exponent)
+	this_gain = sliding_LFP_gain(i,:);
+	this_stim = mean(PID(windows(i)*1e3:1e3*(windows(i)+sliding_window),:));
+	rm_this = isnan(this_stim) | isnan(this_gain);
+	this_gain(rm_this) = []; this_stim(rm_this) =[];
+	try
+		ff = fit(this_stim(:),this_gain(:),'power1'); 
+		sliding_LFP_exponent(i) = ff.b;
+		temp = confint(ff);
+		sliding_LFP_exponent_err(i) = abs(ff.b-temp(1,2));
+	end
+end
+
+
+figure('outerposition',[0 0 1000 500],'PaperUnits','points','PaperSize',[1000 500]); hold on
+errorShade((a:sliding_window:z)+sliding_window/2,sliding_LFP_exponent,sliding_LFP_exponent_err)
+ylabel('Exponent of Weber Fit')
+xlabel('Time (s)')
+
+prettyFig;
+
+if being_published
+	snapnow
+	delete(gcf)
+end
+
 
 %% Version Info
 % The file that generated this document is called:

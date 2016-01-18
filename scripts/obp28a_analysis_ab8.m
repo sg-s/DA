ba21cd9a40a739b312c8d8df7fab16d7507916bf
srinivas.gs <srinivas.gs@yale.edu>
2016-01-18 11:01:05 -0500

    added new B neuron ablation data to obp analysis

--- scripts/obp28a_analysis_ab8.m
+++ scripts/obp28a_analysis_ab8.m
@@ -10,25 +10,7 @@
 % In this document, we see if the gain properties of the ab8A ORN are changed when we delete the OBP 28a. 
 
 
-
-% add homebrew path
-path1 = getenv('PATH');
-if isempty(strfind(path1,':/usr/local/bin'))
-    path1 = [path1 ':/usr/local/bin'];
-end
-setenv('PATH', path1);
-
-% this code determines if this function is being called
-calling_func = dbstack;
-being_published = 0;
-if ~isempty(calling_func)
-	if find(strcmp('publish',{calling_func.name}))
-		being_published = 1;
-		unix(['tag -a publish-failed ',which(mfilename)]);
-		unix(['tag -r published ',which(mfilename)]);
-	end
-end
-tic
+pHeader;
 
 % prep and merge data
 use_cache = 1;
@@ -109,6 +91,7 @@ end
 
 %%
 % In the following figure, we compare the KO to the control genotype.
+
 figure('outerposition',[0 0 1000 800],'PaperUnits','points','PaperSize',[1000 800]); hold on
 c = parula(max(paradigm));
 subplot(3,1,1), hold on
@@ -276,37 +259,135 @@ if being_published
 	delete(gcf)
 end
 
+%% Ablation of the B neuron
+% In an effort to make the spikes more sortable, Nikki ablated the B neuron usind DsArm. Unfortunately, for unknown reasons, the B neuron was not ablated. In the following figure, we try to compare responses from the OBP KO and the wCS control, where we hoped the B neuron was ablated, but in reality, it wasn't. 
 
-%% Version Info
-% The file that generated this document is called:
-disp(mfilename)
 
-%%
-% and its md5 hash is:
-Opt.Input = 'file';
-disp(dataHash(strcat(mfilename,'.m'),Opt))
+% prep and merge data
+use_cache = 1;
+[PID, LFP, fA, paradigm, orn,fly] = consolidateData('/local-data/obp/ab8/B-ablated-ko',use_cache);
+geno = ones(length(orn),1);
 
-%%
-% This file should be in this commit:
-[status,m]=unix('git rev-parse HEAD');
-if ~status
-	disp(m)
+[PID2, LFP2, fA2, paradigm2, orn2,fly2] = consolidateData('/local-data/obp/ab8/B-ablated-wcs',use_cache);
+geno2 = 2*ones(length(orn2),1);
+
+PID = [PID PID2]; clear PID2
+fA = [fA fA2]; clear fA2
+LFP = [LFP LFP2]; clear LFP2
+paradigm = [paradigm paradigm2]; clear paradigm2
+orn = [orn orn2]; clear orn2
+fly = [fly; fly2]; clear fly2
+geno = [geno; geno2]; clear geno2
+paradigm = paradigm(:);
+orn = orn(:);
+
+% clean up data
+rm_this = isnan(sum(LFP));
+fA(:,rm_this) = [];
+PID(:,rm_this) = [];
+LFP(:,rm_this) = [];
+paradigm(rm_this) = [];
+orn(rm_this) = [];
+fly(rm_this) = [];
+geno(rm_this) = [];
+fA(:,sum(fA) == 0) = NaN;
+
+
+% bandPass LFP
+filtered_LFP = LFP;
+for i = 1:width(LFP)
+	filtered_LFP(:,i) = bandPass(LFP(:,i),1000,10);
 end
 
+a = 10e3; z = 50e3;
+[K1,LFP_pred,LFP_gain] = extractFilters(PID,filtered_LFP,'use_cache',true,'a',a,'z',z);
+
+[K2,fA_pred,fA_gain] = extractFilters(PID,fA,'use_cache',true,'a',a,'z',z);
+
 %%
-% This file has the following external dependencies:
-showDependencyHash(mfilename);
+% In the following figure, we compare filters and nonlinearities for the stimulus to LFP transformations. 
 
-t = toc;
+figure('outerposition',[0 0 800 800],'PaperUnits','points','PaperSize',[900 800]); hold on
+filtertime = 1e-3*(1:length(K1)) - .1;
+opacity = .3;
+for i = 1:max(paradigm)
+	subplot(3,2,2*(i-1)+1), hold on
+	this_K = K1(:,paradigm==i & geno == 2);
+	errorShade(filtertime,nanmean(this_K,2),nanstd(this_K'),'Color','k');
+	this_K = K1(:,paradigm==i & geno == 1);
+	errorShade(filtertime,nanmean(this_K,2),nanstd(this_K'),'Color','r');
+	xlabel('Filter Lag (s)')
 
-%% 
-% This document was built in: 
-disp(strcat(oval(t,3),' seconds.'))
+	subplot(3,2,2*(i-1)+2), hold on
+	x = LFP_pred(a:z,paradigm==i & geno == 2);
+	y = filtered_LFP(a:z,paradigm==i & geno == 2);
+	for j = 1:width(x)
+		try
+			[~,data]=plotPieceWiseLinear(x(:,j),y(:,j),'nbins',50,'make_plot',false);
+			h = plot(data.x,data.y);
+			h.Color = [0 0 0 opacity];
+		end
+	end
+	x = LFP_pred(a:z,paradigm==i & geno == 1);
+	y = filtered_LFP(a:z,paradigm==i & geno == 1);
+	for j = 1:width(x)
+		try
+			[~,data]=plotPieceWiseLinear(x(:,j),y(:,j),'nbins',50,'make_plot',false);
+			h = plot(data.x,data.y);
+			h.Color = [1 0 0 opacity];
+		end
+	end
+	xlabel('Linear Prediction (mV)')
+	ylabel('\DeltaLFP(mV)')
+end
 
-% tag the file as being published 
+prettyFig('fs=20;')
 
 if being_published
-	unix(['tag -a published ',which(mfilename)]);
-	unix(['tag -r publish-failed ',which(mfilename)]);
+	snapnow
+	delete(gcf)
 end
 
+%%
+% Now we compare filters and nonlinearities for the PID->firing rate transformation. 
+
+figure('outerposition',[0 0 800 800],'PaperUnits','points','PaperSize',[900 800]); hold on
+filtertime = 1e-3*(1:length(K2)) - .1;
+for i = 1:max(paradigm)
+	subplot(3,2,2*(i-1)+1), hold on
+	this_K = K2(:,paradigm==i & geno == 2);
+	errorShade(filtertime,nanmean(this_K,2),nanstd(this_K'),'Color','k');
+	this_K = K2(:,paradigm==i & geno == 1);
+	errorShade(filtertime,nanmean(this_K,2),nanstd(this_K'),'Color','r');
+	xlabel('Filter Lag (s)')
+
+	subplot(3,2,2*(i-1)+2), hold on
+	x = fA_pred(a:z,paradigm==i & geno == 2);
+	y = fA(a:z,paradigm==i & geno == 2);
+	for j = 1:width(x)
+		try
+			[~,data]=plotPieceWiseLinear(x(:,j),y(:,j),'nbins',50,'make_plot',false);
+			h = plot(data.x,data.y);
+			h.Color = [0 0 0 opacity];
+		end
+	end
+	x = fA_pred(a:z,paradigm==i & geno == 1);
+	y = fA(a:z,paradigm==i & geno == 1);
+	for j = 1:width(x)
+		try
+			[~,data]=plotPieceWiseLinear(x(:,j),y(:,j),'nbins',50,'make_plot',false);
+			h = plot(data.x,data.y);
+			h.Color = [1 0 0 opacity];
+		end
+	end
+	xlabel('Linear Prediction (V)')
+	ylabel('Firing Rate(Hz)')
+end
+
+prettyFig('fs=20;')
+
+
+%% Version Info
+%
+
+pFooter;

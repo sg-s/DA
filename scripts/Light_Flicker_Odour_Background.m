ff9e4a198263e4d0ebb5ecb0a66b1fa0a66704f6
srinivas.gs <srinivas.gs@yale.edu>
2016-03-12 17:01:33 -0500

    finished analysis of light flicker and odour background

--- scripts/Light_Flicker_Odour_Background.m
+++ scripts/Light_Flicker_Odour_Background.m
@@ -39,31 +39,6 @@ if ~exist('od','var')
 
 end
 
-%%
-% First, we show that the background odour activates the ORN. In our experiments, the odour is turned on 5 seconds before the light starts to fluctuate. We plot the ORN response during this initial period as a function of odour concentration. 
-
-figure('outerposition',[100 100 500 500],'PaperUnits','points','PaperSize',[1000 500]); hold on
-c = lines(size(od,1));
-for i = 1:size(od,1)
-	% for each neuron
-	for j = 1:size(od,2)
-		% for each paradigm
-		f = nanmean(od(i,j).firing_rate(1:5e3,:));
-		s = nanmean(od_odour(i,j).stimulus(1:5e3,:));
-		plot(s,f,'+','Color',c(i,:))
-	end
-end
-xlabel('Mean Odour Stimulus (V)')
-ylabel('Odour-induced Firing rate (Hz)')
-
-prettyFig()
-
-if being_published	
-	snapnow	
-	delete(gcf)
-end
-
-
 % figure out paradigm ordering 
 mean_s = NaN(size(od_odour,2),1);
 for i = 1:size(od_odour,2)
@@ -75,12 +50,21 @@ for i = 1:size(od_odour,1)
 	od(i,:) = permute(od(i,:),idx);
 end
 
-%%
-% Now, we back out filters and plot the nonlinearities in the light flicker regime, and plot them colour coded by odour background level.
+% remove the baseline PID from all the PID values
+PID_baseline = nanmean(nanmean([od_odour(:,1).stimulus]));
+for i = 1:size(od_odour,1)
+	od_odour(i,1).stimulus = 0*od_odour(i,1).stimulus + 1e-4; % we will reset this to 0 in the plot
+	for j = 2:size(od_odour,2)
+		od_odour(i,j).stimulus = od_odour(i,j).stimulus - PID_baseline;
+	end
+end
 
-figure('outerposition',[0 0 1000 800],'PaperUnits','points','PaperSize',[1400 800]); hold on
+%%
+% First, we back out filters and plot the nonlinearities in the light flicker regime, and plot them colour coded by odour background level.
+
+figure('outerposition',[0 0 1600 800],'PaperUnits','points','PaperSize',[1600 800]); hold on
 clear ax
-for i = 1:size(od,1)*3
+for i = size(od,1)*3:-1:1
 	ax(i) = subplot(3,size(od,1),i); hold on
 end
 filtertime = 1e-3*(1:700) - .1;
@@ -88,23 +72,23 @@ for i = 1:size(od,1)
 	c = parula(length(find([od(i,:).n_trials]))+1);
 	ci = 1;
 	gain = NaN(size(od,2),1);
-	odour_induced_firing_rate = NaN(size(od,2),1);
+	odour_levels = NaN(size(od,2),1);
 	for j = 1:size(od,2)
 		if od(i,j).n_trials > 0
 			K = (nanmean(od(i,j).K_firing,2));
-			x = (nanmean(od(i,j).firing_projected(uts,:),2));
-			y = (nanmean(od(i,j).firing_rate(uts,:),2));
+			x = (nanmean(od(i,j).firing_projected(uts,:),2)); x = x(:);
+			y = (nanmean(od(i,j).firing_rate(uts,:),2)); y = y(:);
 			plot(ax(i),filtertime,K,'Color',c(ci,:));
 			axes(ax(i+size(od,1)))
 			[~,data] = plotPieceWiseLinear(x,y,'nbins',50,'Color',c(ci,:));
-			middle_segment = (data.y > max(data.y)/3 & data.y < 2*max(data.y)/3);
-			ff = fit(data.x(middle_segment),data.y(middle_segment),'poly1');
+			middle_segment = (y > max(y)/3 & y < 2*max(y)/3);
+			ff = fit(x(middle_segment),y(middle_segment),'poly1');
 			gain(j) = ff.p1;
-			odour_induced_firing_rate(j) = nanmean((nanmean(od(i,j).firing_rate(1:5e3,:),2)));
+			odour_levels(j) = nanmean((nanmean(od_odour(i,j).stimulus(:,:),2)));
 			ci = ci+1;
 		end
 	end
-	plot(ax(i+size(od,1)*2),odour_induced_firing_rate,gain,'k+')
+	plot(ax(i+size(od,1)*2),odour_levels,gain,'k+')
 end
 
 % equalise axes everywhere
@@ -124,7 +108,7 @@ end
 
 for i = size(od,1)*2+1:length(ax)
 	ylabel(ax(i),'Gain (Hz/\muW)')
-	xlabel(ax(i),'Odour-induced firing rate (Hz)')
+	xlabel(ax(i),'Odor Stimulus (V)')
 end
 
 prettyFig('fs=18;')
@@ -133,6 +117,79 @@ if being_published
 	snapnow	
 	delete(gcf)
 end
+
+%%
+% Now, we summarise how the gain changes in all the neurons in one figure:
+
+figure('outerposition',[0 0 500 500],'PaperUnits','points','PaperSize',[1000 500]); hold on
+for i = 1:size(od,1)
+	c = lines(size(od,1));
+	clear gain odour_levels
+	gain = NaN(size(od,2),1);
+	odour_levels = NaN(size(od,2),1);
+	for j = 1:size(od,2)
+		if od(i,j).n_trials > 0
+			K = (nanmean(od(i,j).K_firing,2));
+			x = (nanmean(od(i,j).firing_projected(uts,:),2)); x = x(:);
+			y = (nanmean(od(i,j).firing_rate(uts,:),2)); y = y(:);
+
+			[~,data] = plotPieceWiseLinear(x,y,'nbins',50,'make_plot',false);
+			middle_segment = (y > max(y)/3 & y < 2*max(y)/3);
+			ff = fit(x(middle_segment),y(middle_segment),'poly1');
+			gain(j) = ff.p1;
+			odour_levels(j) = nanmean((nanmean(od_odour(i,j).stimulus(:,:),2)));
+		end
+	end
+	rm_this = isnan(odour_levels) | isnan(gain);
+	odour_levels(rm_this) = []; gain(rm_this) = [];
+	plot(odour_levels,gain,'-+','Color',c(i,:))
+end
+
+set(gca,'XScale','log','YLim',[0 1])
+xlabel('Odour Stimulus (V)')
+ylabel('Gain (Hz/\muW)')
+prettyFig('fs=18;')
+
+if being_published	
+	snapnow	
+	delete(gcf)
+end
+
+
+
+
+%%
+% Now, we show that the background odour activates the ORN. In our experiments, the odour is turned on 5 seconds before the light starts to fluctuate. We plot the ORN response during this initial period as a function of odour concentration. 
+
+figure('outerposition',[100 100 500 500],'PaperUnits','points','PaperSize',[1000 500]); hold on
+c = lines(size(od,1));
+for i = 1:size(od,1)
+	% for each neuron
+	s = NaN(size(od,2),1);
+	f = NaN(size(od,2),1);
+	for j = 1:size(od,2)
+		% for each paradigm
+		f(j) = nanmean(nanmean(od(i,j).firing_rate(1:5e3,:)));
+		s(j) = nanmean(nanmean(od_odour(i,j).stimulus(1:5e3,:)));
+	end
+	rm_this = isnan(s) | isnan(f);
+	s(rm_this) = []; f(rm_this) = [];
+	plot(s,f,'-+','Color',c(i,:))
+end
+set(gca,'XScale','log')
+xlabel('Mean Odour Stimulus (V)')
+ylabel('Odour-induced Firing rate (Hz)')
+
+prettyFig('FixLogX=true;')
+
+if being_published	
+	snapnow	
+	delete(gcf)
+end
+
+
+
+
 
 %% Version Info
 %

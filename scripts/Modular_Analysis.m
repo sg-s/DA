2cd44cdd576e784a9fbfa7a1fc166005dcb052bb
srinivas.gs <srinivas.gs@yale.edu>
2016-03-22 09:38:36 -0400

    modular analysis shows gain control due to mean stimulus

--- scripts/Modular_Analysis.m
+++ scripts/Modular_Analysis.m
@@ -9,10 +9,10 @@
 pHeader;
 
 %% Modular Analysis
-% In this document, we take seriously the hypothesis that the LFP depends only on the stimulus, and the firing rate depends on the LFP, and analyse the data in these modules. 
+% In this document, we take seriously the hypothesis that the LFP depends only on the stimulus, and the firing rate depends on the LFP, and analyze the data in these modules. Furthermore, we assume that the time derivative of the LFP is proportional to the transduction current. Thus, the (time-derivative) of the LFP should be predicted from the stimulus, and the firing rate should be predicted from the time-derivative of the LFP. 
 
 
-%% Weber Fechner Data
+%% Gain control by the mean stimulus
 % In this section, we analyse the LFP to firing rate transformation in the mean shifted gaussian data. In the following figure, we extract filters from the PID, the derivative of the LFP and the firing rate, and compare them. 
 
 
@@ -55,14 +55,14 @@ dLFP = LFP;
 for i = 1:length(paradigm)
 	% first high pass them to remove spikes
 	dLFP(:,i) = 10*bandPass(LFP(:,i),Inf,10);
-	dLFP(:,i) = filtfilt(ones(10,1),10,[0; diff(LFP(:,i))]);
+	dLFP(:,i) = 1e3*filtfilt(ones(10,1),10,[0; diff(LFP(:,i))]);
 end
 
 % extract filters and find gain
 a = 35e3; z = 55e3;
-[K1,K1p] = extractFilters(PID,dLFP,'use_cache',true,'a',a,'z',z);
-[K2,K2p] = extractFilters(dLFP,fA,'use_cache',true,'a',a,'z',z);
-[K3,K3p] = extractFilters(PID,fA,'use_cache',true,'a',a,'z',z);
+[K1,K1p,K1_gain] = extractFilters(PID,dLFP,'use_cache',true,'a',a,'z',z);
+[K2,K2p,K2_gain] = extractFilters(dLFP,fA,'use_cache',true,'a',a,'z',z);
+[K3,K3p,K3_gain] = extractFilters(PID,fA,'use_cache',true,'a',a,'z',z);
 ft = 1e-3*(1:length(K1)) - .1;
 
 
@@ -131,12 +131,94 @@ xlabel('K3 \otimes s(t)')
 ylabel('Firing Rate (Hz)')
 legend(['r^2 = ' oval(rsquare(x,y))],'Location','southeast')
 
-
-
-xlabel('Filtertime (s)')
-
-
 prettyFig()
+if being_published	
+	snapnow	
+	delete(gcf)
+end
+
+%% 
+% Now, we plot how the input-output curves of the dLFP and the firing rate change as a function of the mean stimulus. 
+
+figure('outerposition',[0 0 1200 700],'PaperUnits','points','PaperSize',[1200 700]); hold on
+ss = 100;
+c = parula(max(paradigm)+1);
+mean_stim = nanmean(PID(a:z,:));
+ms = [min(mean_stim) max(mean_stim)];
+
+subplot(2,3,1), hold on
+for i = 1:max(paradigm) % iterate over all paradigms 
+	y = nanmean(dLFP(a:z,paradigm == i),2);
+	x = nanmean(K1p(a:z,paradigm == i),2);
+	plotPieceWiseLinear(x,y,'nbins',50,'Color',c(i,:));
+end
+xlabel('K1 \otimes s(t)')
+ylabel('dLPF (mV/s)')
+
+subplot(2,3,2), hold on
+for i = 1:max(paradigm) % iterate over all paradigms 
+	y = nanmean(fA(a:z,paradigm == i),2);
+	x = nanmean(K2p(a:z,paradigm == i),2);
+	plotPieceWiseLinear(x,y,'nbins',50,'Color',c(i,:));
+end
+xlabel('K2 \otimes dLFP(t)')
+ylabel('Firing Rate (Hz)')
+
+subplot(2,3,3), hold on
+for i = 1:max(paradigm) % iterate over all paradigms 
+	y = nanmean(fA(a:z,paradigm == i),2);
+	x = nanmean(K3p(a:z,paradigm == i),2);
+	x = x - nanmean(x);
+	s = nanmean(PID(a:z,paradigm==i),2);
+	x = x + nanmean(s);
+	plotPieceWiseLinear(x,y,'nbins',50,'Color',c(i,:));
+end
+xlabel('K3 \otimes s(t)')
+ylabel('Firing Rate (Hz)')
+
+subplot(2,3,4), hold on
+for i = 1:length(paradigm)
+	plot(mean_stim(i),K1_gain(i),'+','Color',c(paradigm(i),:))
+end
+xlabel('Mean Stimulus (V)')
+ylabel('dLFP Gain (mV/s/V)')
+set(gca,'XScale','log','YScale','log')
+ff = fit(mean_stim(:),K1_gain(:),'power1','Upper',[Inf -1],'Lower',[0 -1]);
+plot(ms,ff(ms),'r')
+
+
+subplot(2,3,5), hold on
+for i = 1:length(paradigm)
+	plot(mean_stim(i),K2_gain(i),'+','Color',c(paradigm(i),:))
+end
+xlabel('Mean Stimulus (V)')
+ylabel('Firing Gain (Hz/mV/s)')
+set(gca,'XScale','log','YScale','log')
+
+subplot(2,3,6), hold on
+for i = 1:length(paradigm)
+	plot(mean_stim(i),K3_gain(i),'+','Color',c(paradigm(i),:))
+end
+xlabel('Mean Stimulus (V)')
+ylabel('Firing Gain (Hz/V)')
+set(gca,'XScale','log','YScale','log')
+ff = fit(mean_stim(:),K3_gain(:),'power1','Upper',[Inf -1],'Lower',[0 -1]);
+plot(ms,ff(ms),'r')
+prettyFig('fs',18)
+
+if being_published	
+	snapnow	
+	delete(gcf)
+end
+
+%%
+% This clearly demonstrates that gain control to the mean happens at the transduction level, which is responsible for the Weber-Fechner Law at the firing machinery. We can also see that the gain of the firing machinery itself doesn't change with the mean stimulus. 
+
+%% Contrast Sensitivity 
+% In this section we analyse the data where we switch between two contrasts, while keeping the mean stimulus the same. 
+
+
+
 
 
 %% Version Info

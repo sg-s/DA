b74cf397cfeb26ea575023c8ff1954e7855916c0
srinivas.gs <srinivas.gs@yale.edu>
2016-06-23 16:15:18 -0400

    updated supp. fig. for fig 7

--- paper-figures/fig7_kinetics.m
+++ paper-figures/fig7_kinetics.m
@@ -172,7 +172,7 @@ end
 % ##    ## ##     ## ##        ##           ##        ##  ##    ##  
 %  ######   #######  ##        ##           ##       ####  ######   
     
-figure('outerposition',[0 0 1200 801],'PaperUnits','points','PaperSize',[1200 801]); hold on
+figure('outerposition',[0 0 1200 712],'PaperUnits','points','PaperSize',[1200 712]); hold on
 clear ax
 for i = 8:-1:1
 	ax(i) = subplot(2,4,i); hold on
@@ -230,16 +230,23 @@ end
 % fake some plots for a nice legend
 clear l L
 for i = 1:length(markers)-1
-	l(i) = plot(ax(2),NaN,NaN,'Marker',markers{i},'Color','k','LineStyle','none');
+	l(i) = plot(ax(1),NaN,NaN,'Marker',markers{i},'Color','k','LineStyle','none');
 	L{i} = [orn_names{i} ' ' odour_names{i}];
 end
-lh = legend(l,L,'Location','southeast');
-lh.Position = [0.31 0.7 0.15 0.1];
-lh.FontSize = 15;
+lh1 = legend(l,L,'Location','southeast');
+lh1.Position = [0.31 0.7 0.15 0.1];
+lh1.FontSize = 15;
 
 set(ax(1),'YLim',[0 200],'XLim',[-0.1 1.1])
 xlabel(ax(1),'Mean stimulus (rescaled)')
 
+%  ######     ###    ########  ##        #######  ######## ########    ###    
+% ##    ##   ## ##   ##     ## ##       ##     ##    ##       ##      ## ##   
+% ##        ##   ##  ##     ## ##       ##     ##    ##       ##     ##   ##  
+% ##       ##     ## ########  ##       ##     ##    ##       ##    ##     ## 
+% ##       ######### ##   ##   ##       ##     ##    ##       ##    ######### 
+% ##    ## ##     ## ##    ##  ##       ##     ##    ##       ##    ##     ## 
+%  ######  ##     ## ##     ## ########  #######     ##       ##    ##     ## 
 
 %% Now, show the timescale of gain control using Carlotta's data
 
@@ -255,7 +262,7 @@ end
 do_these = [18 7 8 10 14 17 12];
 odour_names = {'1-pentanol','methyl-butyrate','1-octen-3-ol','diethyl-succinate','ethyl-acetate','2-butanone','isoamyl-acetate'};
 
-clear plot_handles
+c = lines(length(do_these));
 for i = 1:length(do_these)
 	temp = orn_data(do_these(i));
 	pred = nanmean(temp.firing_projected,2); pred = pred(temp.use_this_segment);
@@ -282,22 +289,184 @@ for i = 1:length(do_these)
 	% also find rho for various values of the history length and plot it
 	rho = findRhoForHistoryLengths(gain,stim,ons,offs,history_lengths);
 
-	plot_handles(i) = plot(ax(5),history_lengths,rho,'-');
+	plot(ax(5),history_lengths,rho,'-','Color',c(i,:));
 end
 
 set(ax(5),'XScale','log','YLim',[-1 0],'XTick',[1e2 1e3 1e4],'XLim',[100 1e4])
 xlabel(ax(5),'Gain control timescale (ms)')
-ylabel(ax(5),'Spearman''s \rho')
+ylabel(ax(5),['Correlation between' char(10) 'gain and \mu_{stimulus}'])
+
 
 
 % fake some plots for a nice legend
-clear L
-for i = 1:length(plot_handles)
+clear L l
+for i = 1:length(do_these)
+	l(i) = plot(ax(5),NaN,NaN,'Marker','o','MarkerFaceColor',c(i,:),'LineStyle','none');
 	L{i} = [orn_data(do_these(i)).neuron_name ' ' odour_names{i}];
 end
-lh = legend(plot_handles,L,'Location','southeast');
-lh.Position = [0.31 0.15 0.15 0.2];
-lh.FontSize = 15;
+lh2 = legend(l,L,'Location','southeast');
+lh2.Position = [0.31 0.15 0.15 0.2];
+lh2.FontSize = 15;
+
+
+% ########  ######## ########  ##       ####  ######     ###    ######## ########  ######  
+% ##     ## ##       ##     ## ##        ##  ##    ##   ## ##      ##    ##       ##    ## 
+% ##     ## ##       ##     ## ##        ##  ##        ##   ##     ##    ##       ##       
+% ########  ######   ########  ##        ##  ##       ##     ##    ##    ######    ######  
+% ##   ##   ##       ##        ##        ##  ##       #########    ##    ##             ## 
+% ##    ##  ##       ##        ##        ##  ##    ## ##     ##    ##    ##       ##    ## 
+% ##     ## ######## ##        ######## ####  ######  ##     ##    ##    ########  ######  
+
+
+do_these = [7 9 13 15 16];
+c = lines(length(do_these));
+clear l
+for i = 1:length(do_these)
+	temp = orn_data(do_these(i));
+	pred = nanmean(temp.firing_projected,2); pred = pred(temp.use_this_segment);
+	resp = nanmean(temp.firing_rate,2);  resp = resp(temp.use_this_segment);
+	stim = nanmean(temp.stimulus,2); stim = stim(temp.use_this_segment);
+ 	stim = stim/nanmean(stim);
+
+	% find when the valve opens
+	[ons,offs] = findValveWhiffs(temp);
+
+	% plot the gain in each of these windows
+	[gain,gain_err] = findGainInWindows(ons,offs,pred,resp);
+
+	gain = gain/nanmean(gain);
+
+	rm_this = gain<0.4 | gain_err < .8;
+	gain(rm_this) = [];
+	ons(rm_this) = [];
+	offs(rm_this) = [];
+
+	% find the mean stimulus in the preceding X ms in these windows
+	mean_stim = findMeanInWindows(ons,offs,computeSmoothedStimulus(stim,example_history_length));
+
+	% % plot the gain vs. the mean stim after sorting it
+	[mean_stim,idx] = sort(mean_stim);
+	plot(ax(2),mean_stim,gain(idx),'+-','Color',c(i,:));
+
+	% also find rho for various values of the history length and plot it
+	rho = findRhoForHistoryLengths(gain,stim,ons,offs,history_lengths);
+
+	plot(ax(6),history_lengths,rho,'.-','Color',c(i,:))
+end
+
+set(ax(6),'XScale','log','YLim',[-1 0],'XTick',[10 100 1e3 1e4])
+set(ax(2),'XScale','log','YScale','log','XLim',[.1 2],'YLim',[.1 10])
+xlabel(ax(2),['\mu_{Stimulus} in preceding ' oval(example_history_length) 'ms (norm)'])
+ylabel(ax(2),'Gain (norm)')
+xlabel(ax(6),'Gain control timescale (ms)')
+ylabel(ax(6),['Correlation between' char(10) 'gain and \mu_{stimulus}'])
+title(ax(2),['ab3A ORN,' char(10) ' methyl butyrate odorant'])
+
+% fake some plots for a nice legend
+clear l
+for i = 1:length(c)
+	l(i) = plot(ax(2),NaN,NaN,'Marker','o','MarkerFaceColor',c(i,:),'LineStyle','none','MarkerEdgeColor',c(i,:));
+end
+lh3 = legend(l,{'5/28','6/05','6/12','6/19','6/19'},'Location','southwest');
+
+
+
+%  ######  #### ##     ## ##     ## ##          ###    ######## ####  #######  ##    ##  ######  
+% ##    ##  ##  ###   ### ##     ## ##         ## ##      ##     ##  ##     ## ###   ## ##    ## 
+% ##        ##  #### #### ##     ## ##        ##   ##     ##     ##  ##     ## ####  ## ##       
+%  ######   ##  ## ### ## ##     ## ##       ##     ##    ##     ##  ##     ## ## ## ##  ######  
+%       ##  ##  ##     ## ##     ## ##       #########    ##     ##  ##     ## ##  ####       ## 
+% ##    ##  ##  ##     ## ##     ## ##       ##     ##    ##     ##  ##     ## ##   ### ##    ## 
+%  ######  #### ##     ##  #######  ######## ##     ##    ##    ####  #######  ##    ##  ######  
+
+% first, fit a DA model to the methyl butyrate ab3A data
+clear p
+p.   s0 = -0.0011;
+p.  n_z = 2;
+p.tau_z = 50; % rounded off
+p.  n_y = 2;
+p.tau_y = 15.4531;
+p.    C = 0.0126;
+p.    A = 2.5013e+04;
+p.    B = 384.5000;
+
+% generate synthetic data responses and do the analyses on that
+
+% tweak the model's tau_gain
+tau_gain = [50 100 200 400];
+stim = mean(orn_data(16).stimulus,2);
+stim = stim(30e3:end);
+stim = stim/nanmean(stim);
+
+
+c = parula(length(tau_gain)+1);
+clear l
+for i = 1:length(tau_gain)
+	p.tau_z = tau_gain(i);
+
+	% generate responses
+	resp = DAModelv2(stim,p);
+
+	% fit a filter to this
+	K = fitFilter2Data(stim,resp,'filter_length',1e3,'offset',200);
+	K = K(100:end-100);
+	filtertime = (1:length(K)) - 100;
+	pred = convolve(1:length(stim),stim,K,filtertime);
+
+
+	% plot the gain in each of these windows
+	[gain,gain_err] = findGainInWindows(ons,offs,pred,resp);
+
+	gain = gain/nanmean(gain);
+
+	rm_this = gain<0.4 | gain_err < .8;
+	gain(rm_this) = [];
+	ons(rm_this) = [];
+	offs(rm_this) = [];
+
+	% find the mean stimulus in the preceding X ms in these windows
+	mean_stim = findMeanInWindows(ons,offs,computeSmoothedStimulus(stim,example_history_length));
+
+	% % plot the gain vs. the mean stim after sorting it
+	[mean_stim,idx] = sort(mean_stim);
+	l(i) = plot(ax(3),mean_stim,gain(idx),'+-','Color',c(i,:));
+
+	% also find rho for various values of the history length and plot it
+	rho = findRhoForHistoryLengths(gain,stim,ons,offs,history_lengths);
+
+	plot(ax(7),history_lengths,rho,'.-','Color',c(i,:))
+end
+set(ax(7),'XScale','log','YLim',[-1 0],'XTick',[10 100 1e3 1e4])
+set(ax(3),'XScale','log','YScale','log','XLim',[.1 2],'YLim',[.05 10])
+title(ax(3),['Simulations with ' char(10) 'varying \tau_{gain}'])
+xlabel(ax(3),'\mu_{Stimulus} in preceding 300ms (V)')
+
+clear l L
+for i = 1:length(tau_gain)
+	l(i) = plot(ax(3),NaN,NaN,'Marker','o','MarkerFaceColor',c(i,:),'LineStyle','none','MarkerEdgeColor',c(i,:));
+	L{i} = ['\tau_{gain} = ' oval(4*tau_gain(i)) 'ms'];
+end 
+lh4 = legend(l,L);
+lh4.Position = [0.795   0.62    0.038    0.08];
+xlabel(ax(7),'Gain control timescale (ms)')
+ylabel(ax(7),['Correlation between' char(10) 'gain and \mu_{stimulus}'])
+
+prettyFig('fs',14);
+
+if being_published
+	snapnow
+	delete(gcf)
+end
+
+% move some plots
+ax(3).Position(1) = .76;
+ax(7).Position(1) = .76;
+
+
+
+%% Version Info
+%
+pFooter;
 
 
 

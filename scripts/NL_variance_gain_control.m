7da6b5e0c0edb519fade7825aa9482f776c738a6
srinivas.gs <srinivas.gs@yale.edu>
2016-12-02 13:24:17 -0500

    fixed #361

--- scripts/NL_variance_gain_control.m
+++ scripts/NL_variance_gain_control.m
@@ -5,11 +5,13 @@ pHeader;
 % In this document, I check if a simple NL model can lead to something that we think of as variance gain control (along the lines of Nemenman's paper). 
 
 %%
-% First, I construct a nonlinearity that looks like this (it's a simple Hill function):
+% First, I construct a nonlinearity that looks like this (it's a simple Hill function with n = 8):
+
+hill_param = [1 5 8];
 
 figure('outerposition',[0 0 500 500],'PaperUnits','points','PaperSize',[1000 500]); hold on
 x = linspace(0,10,100);
-H = hill([1 5 8],x);
+H = hill(hill_param,x);
 plot(x,H,'k')
 xlabel('Stimulus')
 ylabel('input nonlinearity')
@@ -24,7 +26,7 @@ end
 % Then, I consider a stimulus that varies from [4 6] and another that varies from [2 8]. I feed this stimulus through the nonlinearity, and then through a simple filter. 
 
 S = [(4+2*rand(1e4,1)); (2+6*rand(1e4,1))];
-x = hill([1 5 8],S);
+x = hill(hill_param,S);
 p.A = .3; p.tau1 = 20; p.tau2 = 70; p.n = 2;
 K = filter_gamma2(1:1e3,p);
 R = filter(K,1,x);
@@ -76,6 +78,42 @@ if being_published
 	snapnow	
 	delete(gcf)
 end
+
+%%
+% So it looks like it is possible for a NL model to show what we measure as variance gain control using a LN model. How does this vary with the steepness of the input nonlinearity? In the following figure, I vary the steepness of the input nonlinearity, keeping everything else the same, and fit LN models to the data to estimate effective variance gain control as a function of input steepness. 
+
+all_n = [1 2 4 8 16 32];
+
+figure('outerposition',[0 0 1200 802],'PaperUnits','points','PaperSize',[1200 802]); hold on
+for i = 1:length(all_n)
+	% construct the NL model
+	hill_param(3) = all_n(i);
+	x = hill(hill_param,S);
+	R = filter(K,1,x);
+
+	% fit a LN model
+	Khat = fitFilter2Data(S,R,'reg',0);
+	Rhat = filter(Khat,1,S);
+
+	subplot(2,3,i); hold on
+	plotPieceWiseLinear(Rhat(1e3:5e3),R(1e3:5e3),'Color','b','nbins',25);
+	plotPieceWiseLinear(Rhat(6e3:end),R(6e3:end),'Color','r','nbins',25);
+	xlabel('Projected Stimulus')
+	ylabel('Response')
+	title(['n = ' oval(all_n(i))])
+
+end
+
+prettyFig()
+
+if being_published	
+	snapnow	
+	delete(gcf)
+end
+
+%%
+% So it looks like the input nonlinearity has be steep enough for this to work. At low n, Hill functions don't seem to be able to reproduce what we see (curves with different slopes that intersect one another). 
+
 
 
 %% Version Info
